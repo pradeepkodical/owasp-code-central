63ae059967a452f8a83e0c522f909cbc93cca9b4
dinis.cruz <dinis.cruz@18c58f50-7221-0410-b570-0dda8d89c68f>
2006-12-19 17:49:59 +0000

    * Bug: Fixed Search engine for XSTL (now the search works properly and the page is
    automatically scrolled to the selected text)
    	* Feature: XSD verification for Project Xml files. 
    		* Implemented new utils.xml.xsdVerification which verifies a given XML against the 
    
    main project's XSD with the following added funcionlity: setLabelBasedOnResult and 
    
    showMessageBoxWithVerificationResult

--- trunk/labs/ReportGenerator/ascx/ascxProjects.cs
+++ trunk/labs/ReportGenerator/ascx/ascxProjects.cs
@@ -27,6 +27,7 @@ namespace Owasp.VulnReport
 		private System.Windows.Forms.Label lbUnsavedData;
 		private AxXMLSPYPLUGINLib.AxAuthentic axAuthentic_Project;
 		private System.Windows.Forms.Label lblProjectsSaved;
+        private Label lbXmlBreaksXsdSchema;
 		/// <summary> 
 		/// Required designer variable.
 		/// </summary>
@@ -68,20 +69,21 @@ namespace Owasp.VulnReport
             this.lbUnsavedData = new System.Windows.Forms.Label();
             this.btSaveProjectMetadata = new System.Windows.Forms.Button();
             this.axAuthentic_Project = new AxXMLSPYPLUGINLib.AxAuthentic();
+            this.lbXmlBreaksXsdSchema = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axAuthentic_Project)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.lblProjectsSaved);
+            this.groupBox1.Controls.Add(this.lbXmlBreaksXsdSchema);
             this.groupBox1.Controls.Add(this.lbUnsavedData);
             this.groupBox1.Controls.Add(this.btSaveProjectMetadata);
-            this.groupBox1.Location = new System.Drawing.Point(8, 8);
+            this.groupBox1.Location = new System.Drawing.Point(250, 8);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(584, 64);
+            this.groupBox1.Size = new System.Drawing.Size(342, 64);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
             // 
@@ -90,9 +92,9 @@ namespace Owasp.VulnReport
             this.lblProjectsSaved.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblProjectsSaved.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProjectsSaved.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.lblProjectsSaved.Location = new System.Drawing.Point(368, 24);
+            this.lblProjectsSaved.Location = new System.Drawing.Point(124, 15);
             this.lblProjectsSaved.Name = "lblProjectsSaved";
-            this.lblProjectsSaved.Size = new System.Drawing.Size(56, 24);
+            this.lblProjectsSaved.Size = new System.Drawing.Size(56, 37);
             this.lblProjectsSaved.TabIndex = 8;
             this.lblProjectsSaved.Text = "Projects Saved";
             this.lblProjectsSaved.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -103,9 +105,9 @@ namespace Owasp.VulnReport
             this.lbUnsavedData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lbUnsavedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbUnsavedData.ForeColor = System.Drawing.Color.Red;
-            this.lbUnsavedData.Location = new System.Drawing.Point(368, 24);
+            this.lbUnsavedData.Location = new System.Drawing.Point(119, 16);
             this.lbUnsavedData.Name = "lbUnsavedData";
-            this.lbUnsavedData.Size = new System.Drawing.Size(56, 24);
+            this.lbUnsavedData.Size = new System.Drawing.Size(65, 40);
             this.lbUnsavedData.TabIndex = 7;
             this.lbUnsavedData.Text = "Unsaved Data";
             this.lbUnsavedData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -115,7 +117,7 @@ namespace Owasp.VulnReport
             // 
             this.btSaveProjectMetadata.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btSaveProjectMetadata.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btSaveProjectMetadata.Location = new System.Drawing.Point(432, 16);
+            this.btSaveProjectMetadata.Location = new System.Drawing.Point(190, 16);
             this.btSaveProjectMetadata.Name = "btSaveProjectMetadata";
             this.btSaveProjectMetadata.Size = new System.Drawing.Size(136, 40);
             this.btSaveProjectMetadata.TabIndex = 3;
@@ -134,6 +136,19 @@ namespace Owasp.VulnReport
             this.axAuthentic_Project.Size = new System.Drawing.Size(584, 296);
             this.axAuthentic_Project.TabIndex = 16;
             this.axAuthentic_Project.SelectionChanged += new System.EventHandler(this.axAuthentic_Project_SelectionChanged);
+            // 
+            // lbXmlBreaksXsdSchema
+            // 
+            this.lbXmlBreaksXsdSchema.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbXmlBreaksXsdSchema.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbXmlBreaksXsdSchema.ForeColor = System.Drawing.Color.Red;
+            this.lbXmlBreaksXsdSchema.Location = new System.Drawing.Point(14, 12);
+            this.lbXmlBreaksXsdSchema.Name = "lbXmlBreaksXsdSchema";
+            this.lbXmlBreaksXsdSchema.Size = new System.Drawing.Size(105, 40);
+            this.lbXmlBreaksXsdSchema.TabIndex = 7;
+            this.lbXmlBreaksXsdSchema.Text = "Note: Xml breaks XSD schema!";
+            this.lbXmlBreaksXsdSchema.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbXmlBreaksXsdSchema.Visible = false;            
             // 
             // ascxProjects
             // 
@@ -175,6 +190,7 @@ namespace Owasp.VulnReport
 
 		private void btSaveProjectMetadata_Click(object sender, System.EventArgs e)
 		{
+            
             setCurrentProjectsFindingIdValue();
             saveCurrentProject();
             setCurrentProjectNumberValue();
@@ -182,6 +198,9 @@ namespace Owasp.VulnReport
 			lblProjectsSaved.Visible= true;
 			lbUnsavedData.Visible= false;
             unsavedDataExists = false;
+
+            // Verify file against schema
+            new utils.xml.xsdVerification().verifyFilePopulateLabelAndShowMessage(strFullPathToCurrentProjectXmlFile, lbXmlBreaksXsdSchema);
 		}
 
         /// <summary>
@@ -235,12 +254,11 @@ namespace Owasp.VulnReport
                 }
 
             }
-            catch (System.Runtime.InteropServices.COMException)
-            {
-                // Swallow these errors.  This is from having stuff like an invalid range.
-                // Since there is no way to validate the range in the authentic control 
-                // we have to do this for right now, YUCK!
-            }
+            // Swallow these errors.  This is from having stuff like an invalid range.
+            // Since there is no way to validate the range in the authentic control 
+            // we have to do this for right now, YUCK!
+            catch (System.Runtime.InteropServices.COMException) { }
+            catch (System.FormatException) {}
 			return -1;			
 		}			
 
@@ -313,5 +331,7 @@ namespace Owasp.VulnReport
                 promptToSave();
             }
         }
+
+     
 	}
 }
--- trunk/labs/ReportGenerator/utils/xml.cs
+++ trunk/labs/ReportGenerator/utils/xml.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Net;
 using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Xsl;
 using System.Xml.XPath;
 using System.Security.Policy;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Owasp.VulnReport.utils
 {
@@ -15,6 +18,7 @@ namespace Owasp.VulnReport.utils
 		public xml()
 		{
 		}
+
 		public static string returnXmlXslTransformation(string sXmlPath, string sXslPath, string sTargetFilePath)
 		{     
 			XmlTextReader xtrXslFile = new XmlTextReader(sXslPath);
@@ -58,5 +62,71 @@ namespace Owasp.VulnReport.utils
 			xtrXslFile.Close();
 			return "";
 		}
+
+        public class xsdVerification
+        {
+            private UserProfile upCurrentUser = UserProfile.GetUserProfile();
+            private OrgBasePaths obpPaths = OrgBasePaths.GetPaths();
+
+            string strXmlValidationResult = "";
+            bool bNoVerificationErrors = false;
+
+            public void verifyXmlFile(string strPathToFileToValidate)
+            {
+                XmlTextReader xtrFileToValidate = new XmlTextReader(strPathToFileToValidate);
+                XmlReaderSettings xrs = new XmlReaderSettings();
+                xrs.Schemas.Add("vuln_report", obpPaths.ProjectSchemaPath);
+                xrs.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(xvrValidator_ValidationEventHandler);
+                xrs.ValidationType = ValidationType.Schema;                
+                
+                XmlReader xrValidator = XmlReader.Create(strPathToFileToValidate, xrs);
+
+                bNoVerificationErrors = true;
+                while (xrValidator.Read()) { }
+                xrValidator.Close();                
+            }
+
+            private void xvrValidator_ValidationEventHandler(Object sender, ValidationEventArgs args)
+            {
+                bNoVerificationErrors = false;
+                strXmlValidationResult += "   - " + args.Message + Environment.NewLine;
+            }
+
+            private void copyXSDToDirectory(string strTargetDirectory, string cbPathXsdtoUseOnExport)
+            {
+                string strPathToXsdFile = Path.Combine(obpPaths.XsdFilePath, cbPathXsdtoUseOnExport);
+                string strPathToDestinationXsdFile = Path.Combine(strTargetDirectory, Path.GetFileName(strPathToXsdFile));
+                File.Copy(strPathToXsdFile, strPathToDestinationXsdFile, true);
+            }
+
+            public string StrXmlValidationResult
+            {
+                get
+                {
+                    return strXmlValidationResult;
+                }
+            }
+
+            public void setLabelBasedOnResult(Label lbLabelToSet)
+            {
+                if (true == bNoVerificationErrors)                
+                    lbLabelToSet.Visible = false;                
+                else
+                    lbLabelToSet.Visible = true;
+            }
+
+            public void showMessageBoxWithVerificationResult()
+            {
+                if (false == bNoVerificationErrors)
+                    MessageBox.Show("Xml Validation Failed:" + Environment.NewLine + Environment.NewLine + this.strXmlValidationResult);
+            }
+
+            public void verifyFilePopulateLabelAndShowMessage(string strPathToFileToValidate, Label lbLabelToSet)
+            {
+                this.verifyXmlFile(strPathToFileToValidate);
+                this.setLabelBasedOnResult(lbLabelToSet);
+                this.showMessageBoxWithVerificationResult();
+            }
+        }        
 	}
 }
