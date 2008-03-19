9391b5aecd389c07c495b1e13b4fa67d6008eb45
leven101 <leven101@18c58f50-7221-0410-b570-0dda8d89c68f>
2008-03-19 13:00:08 +0000

    Communication infrastructure test

--- trunk/labs/SiteGenerator/OSG_new/Owasp.Osg.Communicator/osgCommBuffer.cs
+++ trunk/labs/SiteGenerator/OSG_new/Owasp.Osg.Communicator/osgCommBuffer.cs
@@ -1,37 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Runtime.Remoting;
+using System.Runtime.Remoting.Lifetime;
 
 namespace Owasp.Osg.Communicator
 {
-    class osgCommBuffer: MarshalByRefObject
-    {
-      private bool b_request_; 
-      private bool b_response_;
+	// delegate for the control to set functionality
+	public delegate osgResponse delResponse(osgRequest request);
 
-      public bool requestIn {
-        get {
-          return b_request_;
-        }
-        set {
-          b_request_ = value;
-        }
-      }
-      public bool responseReady {
-        get {
-          return b_response_;
-        }
-        set {
-          b_response_ = value;
-        }
-      }
-      public osgRequest newRequest() {
-        return new osgRequest();
-      }
-      public osgResponse newResponse() {
-        return new osgResponse();
-      }
-         
+  public class osgCommBuffer: MarshalByRefObject
+  {
+    private bool b_request_; 
+    private bool b_response_;
+		private static delResponse delRespond_;
+
+    public osgCommBuffer() {
+      b_request_ = false;
+      b_response_ = false;
     }
+
+		public static delResponse delRespond {
+			get { return delRespond_; }
+			set { delRespond_ = value; }
+		}
+
+		public osgResponse controlResponse(osgRequest request) {
+			request.RequestURI.ToString();
+			requestIn = true;
+			return delRespond_(request);
+		}
+
+    public bool requestIn {
+      get {
+        return b_request_;
+      }
+      set {
+        b_request_ = value;
+      }
+    }
+
+    public bool responseReady {
+      get {
+        return b_response_;
+      }
+      set {
+        b_response_ = value;
+      }
+    }
+
+		public override object InitializeLifetimeService() {
+			//override 5min remote lease time to 30sec
+      ILease lease = (ILease) base.InitializeLifetimeService();
+      lease.InitialLeaseTime = new TimeSpan(0, 0, 30);
+      return lease;
+    }
+  }
 }
