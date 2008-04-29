d013b52003e84ad26756575e7c30b348423151cf
axleyjc <axleyjc@18c58f50-7221-0410-b570-0dda8d89c68f>
2008-04-29 06:42:39 +0000

    Refactored CSRFGuard to be in complete control of all guarding.  Really simplified the csrfguardmodule Implemented plugin architecture for Actions Added nant build for .net 1.1 assemblies. Deleted vs 2005 user file

--- labs/CSRFGuard/CSRFGuard.cs
+++ labs/CSRFGuard/CSRFGuard.cs
@@ -1,7 +1,11 @@
 using System;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.SessionState;
+using System.Diagnostics;
+using org.owasp.csrfguard.Actions;
+using System.Reflection;
 
 namespace org.owasp.csrfguard
 {
@@ -25,53 +29,26 @@ namespace org.owasp.csrfguard
             _session = _httpApp.Session;
 			_context = _httpApp.Context;
 			_response = _httpApp.Context.Response;
-			
-			// If no token yet, this is the first request so set one to be used on the response and then set the flag to indicate checking is not required
-			if (CsrfSessionTokenValue == null)
-			{
-				CsrfSessionTokenValue = Util.generateToken(16);	// 128 random bytes
-				_skipDetect = true;
-			}
-			
-			if (CsrfSessionTokenName == null)
-			{
-				if (App.Configuration.useRandomCSRFTokenName)
-				{
-					CsrfSessionTokenName = Util.generateToken(8);	// 64 random bytes
-				} else
-				{
-					if (App.Configuration.staticCSRFTokenName != null)
-					{
-						CsrfSessionTokenName = App.Configuration.staticCSRFTokenName;	
-					} 
-					else
-					{
-						CsrfSessionTokenName = App.Configuration.defaultCSRFTokenName;						
-					}
-				}
-			}
-			
-			// ignore requests to URLs when no parameters are passed as this would not represent a CSRF attack in most cases (TODO:  make this a configurable option)
-			if (!formOrQueryStringParamsPassed() && App.Configuration.skipDetectOnParameterlessURLRequests)
-			{
-				_skipDetect = true;
-			}
-			
-			// ignore URLs that don't represent CSRF risk (e.g. a request for a GIF)
-			// SECURITY NOTE:  DO NOT USE THE FULL URL, only FilePath, else attackers could pad arbitrary chars to the URL parameters to fool the regex.
-			if (URLHasWhitelistedExtension(_context.Request.FilePath))
-			{
-				_skipDetect = true;
-			}
-			
-			// TODO:  Allow specifying a list of relative URLs to bypass detection on								
-			// ArrayList test = App.Configuration.skipDetectForTheseURLs;
+
+            setupCSRFTokenNameAndValue();
+
+            // do we need to do anything?  Configure the object options.
+            setupSkipDetect();
 			
 			// Check for CSRF and store the results in object properties for later retrieval
-			detectCSRFAttempt();
+            if (!SkipDetect)
+            {
+                detectCSRFAttempt();
+            }
+
+            // take action
+            if (AttackDetected)
+            {
+                handleCSRFAttempt();
+            }
 		}
 		
-		#region Properties
+        // properties 
 		public String CsrfSessionTokenValue
 		{
 			get
@@ -129,28 +106,151 @@ namespace org.owasp.csrfguard
                 return _session;
             }
         }
-		#endregion
 		
-		#region Methods
+		// methods
+
+        /// <summary>
+        /// Check the csrf token name and value in the current asp.net session and create one if not existing already
+        /// </summary>
+        private void setupCSRFTokenNameAndValue()
+        {
+            // If no token yet, this is the first request so set one to be used on the response and then set the flag to indicate checking is not required
+            if (CsrfSessionTokenValue == null)
+            {
+                CsrfSessionTokenValue = Util.generateToken(16);	// 128 random bytes
+                _skipDetect = true;
+            }
+
+            if (CsrfSessionTokenName == null)
+            {
+                if (App.Configuration.useRandomCSRFTokenName)
+                {
+                    CsrfSessionTokenName = Util.generateToken(8);	// 64 random bytes
+                }
+                else
+                {
+                    if (App.Configuration.staticCSRFTokenName != null)
+                    {
+                        CsrfSessionTokenName = App.Configuration.staticCSRFTokenName;
+                    }
+                    else
+                    {
+                        CsrfSessionTokenName = App.Configuration.defaultCSRFTokenName;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Applies rules to determine whether we need to skip detection, based on config items and the request properties.
+        /// </summary>
+        private void setupSkipDetect()
+        {
+            // ignore requests to URLs when no parameters are passed as this would not represent a CSRF attack in most cases (TODO:  make this a configurable option)
+            if (!formOrQueryStringParamsPassed() && App.Configuration.skipDetectOnParameterlessURLRequests)
+            {
+                _skipDetect = true;
+            }
+
+            // ignore URLs that don't represent CSRF risk (e.g. a request for a GIF)
+            // SECURITY NOTE:  DO NOT USE THE FULL URL, only FilePath, else attackers could pad arbitrary chars to the URL parameters to fool the regex.
+            if (URLHasWhitelistedExtension(_context.Request.FilePath))
+            {
+                _skipDetect = true;
+            }
+
+            // TODO:  Allow specifying a list of relative URLs to bypass detection on								
+            // ArrayList test = App.Configuration.skipDetectForTheseURLs;
+        }
+
 		// analyzes the session to detect a CSRF attack.
-		public void detectCSRFAttempt()
-		{
-			// check only when URL is not in the whitelist pattern
-			if (!SkipDetect)
-			{
-				_attackDetected = true;	// fail safe
-						
-				// TODO:  data input validation on Request value
-				// Does the request have a CSRF token embedded?  Does it match the one in session?  If not, we caught an attack.
-				if (_context.Request[CsrfSessionTokenName] != null)
-				{
-					if (CsrfSessionTokenValue == _context.Request[CsrfSessionTokenName])
-					{
-						_attackDetected = false;
-					}
-				}
-			}
-		}
+        private void detectCSRFAttempt()
+        {
+            _attackDetected = true;	// fail safe
+
+            // TODO:  data input validation on Request value
+            // Does the request have a CSRF token embedded?  Does it match the one in session?  If not, we caught an attack.
+            if (_context.Request[CsrfSessionTokenName] != null)
+            {
+                if (CsrfSessionTokenValue == _context.Request[CsrfSessionTokenName])
+                {
+                    _attackDetected = false;
+                }
+            }
+        }
+
+        /**
+        * Handle a detected CSRF attepmpt.  This will use the configuration for what to do next (e.g. redirect, kill session, log, etc.)
+        **/
+        void handleCSRFAttempt()
+        {
+
+            // instantiate the dynamic classes specified in the config file, or the default.  Then tell it to take action.
+            foreach (string hclass in App.Configuration.CSRFHandlers)
+            {
+                EventLog log = new EventLog();
+                #region debug logging
+                log.Source = "Application";
+                log.WriteEntry("DEBUG:  Executing Action " + hclass, EventLogEntryType.Error);
+                #endregion
+                try
+                {
+                    Type type = Type.GetType(hclass, true);
+                    ICSRFHandler handler = Activator.CreateInstance(type) as ICSRFHandler;
+                    handler.Initialize(_httpApp);
+                    handler.TakeAction();
+                }
+                catch (TypeLoadException e)
+                {
+                    log.WriteEntry("Got exception " + e.Message, EventLogEntryType.Error);
+                }
+            }
+
+            // TODO:  support plugins by looking for anything that implements ICSRFHandler interface in a given plugin directory
+            try
+            {
+                string[] pluginFiles = Directory.GetFiles(App.Configuration.CSRFHandler_pluginFolder);
+                foreach (string file in pluginFiles)
+                {
+                    Type objType = null;
+                    try
+                    {
+                        // load the plugin assembly
+                        Assembly plugin = Assembly.Load(file);
+                        if (plugin != null)
+                        {
+                            objType = plugin.GetType(file, true);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    try
+                    {
+                        // now, instantiate the object and do stuff.
+                        if (objType != null)
+                        {
+                            ICSRFHandler handlerPlugin = Activator.CreateInstance(objType) as ICSRFHandler;
+                            handlerPlugin.Initialize(_httpApp);
+                            handlerPlugin.TakeAction();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw;
+            }
+
+            _response.End();
+        }
+
+        // Utility methods
 		
 		// checks the request URL path (without parameters!!!) for whether it matches a whitelist of file extensions to ignore
 		private bool URLHasWhitelistedExtension(String filePath)
@@ -173,6 +273,5 @@ namespace org.owasp.csrfguard
 			}
 			return true;
 		}
-		#endregion
 	}
 }
--- labs/CSRFGuard/CSRFGuardModule.cs
+++ labs/CSRFGuard/CSRFGuardModule.cs
@@ -23,21 +23,12 @@ namespace org.owasp.csrfguard
 			httpApp.ReleaseRequestState += new EventHandler(filterHTMLResponse);
 		}
 		
-		// when the request comes in, decide whether it needs to be handled and then how
+		// when the request comes in, decide whether it needs to be handled and then how.  The Guard does it all.
 		void handleRequest(object sender, EventArgs args)
 		{
 			_guard = new CSRFGuard(sender);
-            _session = new Token(_guard.CsrfSessionTokenName, _guard.CsrfSessionTokenValue);  // this is not accessible in later stages so is saved for access later
-			
-			// this should ignore requests by extension so it won't flag image or CSS requests -- basically, anything that could not be used for CSRF
-			// The object does this checking when instantiated so we just need to ask the object what to do...
-			if (!_guard.SkipDetect)
-			{
-				if (_guard.AttackDetected)
-				{
-					handleCSRFAttempt(sender);
-				}
-			}
+            // save session for later use when filtering the response
+            _session = new Token(_guard.CsrfSessionTokenName, _guard.CsrfSessionTokenValue);
 		}
 		
 		// move this to a utility function
@@ -52,48 +43,15 @@ namespace org.owasp.csrfguard
 			{
 				if(response.ContentType.StartsWith("text/html"))
 				{
+                    // TODO:  this needs to load the filter from the config
 					response.Filter = new RegExFilter(response.Filter, _session.Name, _session.Value);
 				}
 			}
-			catch (Exception e)
+			catch (Exception)
 			{
 				// do something
+                throw;
 			}
-		}
-		
-		/**
-		 * Handle a detected CSRF attepmpt.  This will use the configuration for what to do next (e.g. redirect, kill session, log, etc.)
-		 **/
-
-        // TODO:  move this cruft into the CSRFguard.  It should be doing the protection there.
-		void handleCSRFAttempt(object sender)
-		{
-            HttpApplication _httpApp;
-            HttpContext _context;
-            HttpResponse _response;
-
-            _httpApp = (HttpApplication)sender;
-            _context = _httpApp.Context;
-            _response = _httpApp.Context.Response;
-            // instantiate the dynamic classes specified in the config file, or the default.  Then tell it to take action.
-            foreach (string hclass in App.Configuration.CSRFHandlers)
-            {
-                EventLog log = new EventLog();
-                log.Source = "Application";
-                log.WriteEntry("Executing Action " + hclass, EventLogEntryType.Error);
-                try
-                {
-                    Type type = Type.GetType(hclass, true);
-                    ICSRFHandler handler = Activator.CreateInstance(type) as ICSRFHandler;
-                    handler.Initialize(sender);
-                    handler.TakeAction();
-                }
-                catch (TypeLoadException e)
-                {
-                    log.WriteEntry("Got exception " + e.Message, EventLogEntryType.Error);
-                }
-            }
-            _response.End();
 		}
 
 		public void Dispose()
